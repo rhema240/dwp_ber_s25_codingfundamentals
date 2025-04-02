43aaf5eb81d78a501a498e3aa653f935b6d35531
jsitumorang <jacklyn.situmorang@mailbox.org>
2025-04-02 18:48:04 +0000

    Add L5 material

---
+++ in-class-exercises/lesson-5/exercise-pt1.py
@@ -0,0 +1,33 @@
+'''
+EXERCISE 1
+
+Write a function `greeting` that takes 2 arguments (first_name and last_name) and prints the following message using the given arguments:
+    "Hello, Alice Smith, are you ready for some fun coding today?"
+'''
+
+# solution for exercise 1 👇🏽
+
+
+
+
+'''
+EXERCISE 2
+
+Write a function `repeat_character` that takes a character and a number as arguments and prins a string,
+where the character is repeated the specified number of times. 
+The number has a default value of 2.
+For example, if the character is 'X' and the number is 4, the function should return "XXXX."
+'''
+
+# solution for exercise 2 👇🏽
+
+
+
+'''
+EXERCISE 3
+
+Copy and paste your solution from exercise 2. 
+This time, modify the function so that, if the given number is bigger than 10, it will print out "Sorry, too long!"
+'''
+
+# solution for exercise 3 👇🏽
---
+++ in-class-exercises/lesson-5/exercise-pt2.py
@@ -0,0 +1,11 @@
+'''
+EXERCISE 4
+
+Create a simple dice roll simulator where you use the randint function to simulate rolling a six-sided die. Follow these steps:
+    1. Import the random module to use the randint function.
+    2. Create a function that uses randint function to generate a random integer between 1 and 6, and returns the integer.	
+    3. Store the function result in a variable
+    4. Print out the variable
+'''
+
+# solution for exercise 4 👇🏽
